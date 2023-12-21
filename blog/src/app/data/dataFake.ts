e541962b0883556921763847bfeb18e6d766e7f6
Adriano Santana <adrianobs44@gmail.com>
2023-12-21 16:56:46 -0300

    Versão beta

---
+++ blog/src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "Sobre mim",
+    "description": "marvel anuncia um novo filme do homem de ferro, confira",
+    "photoCover":"ripcut-delivery.disney-plus.net/v1/variant/disney/7F51FA9F6CBD9F0C9B1394B1CC0A6A842D07091318674E234CD33CBF7C28CDC3/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id":"2",
+    "title": "Experiências",
+    "description": "bla blabla",
+    "photoCover":""
+  }
+  
+]
